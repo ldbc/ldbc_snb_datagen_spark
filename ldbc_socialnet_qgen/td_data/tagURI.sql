a10de1ae09dde6174bb80d911531dafbe8846ce1
Mirko Spasic <mirkospasic@gmail.com>
2013-10-08 14:15:10 +0200

    TagURI.sql script

--- ldbc_socialnet_qgen/td_data/tagURI.sql
+++ ldbc_socialnet_qgen/td_data/tagURI.sql
@@ -1,6 +1,10 @@
-sparql select distinct(?tag)
+sparql select ?tag count(*)
 from <sib>
 where {
    ?post a snvoc:Post.
    ?post snvoc:hasTag ?tag .
-};
+}
+group by ?tag
+order by desc 2
+limit 100
+;
