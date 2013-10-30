221368e2402d42d8655a67cd79d7401e8be86176
Mirko Spasic <mirkospasic@gmail.com>
2013-10-30 13:24:16 +0100

    Execution script

---
+++ ldbc_socialnet_qgen/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+basedir=${0%/*}
+libdir=${basedir}/lib
+SEP=':'
+
+if [ -z "$CLASSPATH" ]
+then
+  CLASSPATH=${basedir}/bin  # for development version only
+else
+  CLASSPATH=$CLASSPATH$SEP${basedir}/bin  # for development version only
+fi    
+
+if [ "$(uname)" = "Cygwin" ]; then SEP=';'; fi
+for jar in $libdir/*.jar
+do
+  if [ ! -e "$jar" ]; then continue; fi
+  CLASSPATH="$CLASSPATH$SEP$jar"
+done
+
+export CLASSPATH
+
+./sibdriver -t 300000 -idir td_data -uqp query -uc sib/int -mt 1 -runs 1 http://localhost:8608/sparql -printres -q -defaultparams -rpvq
