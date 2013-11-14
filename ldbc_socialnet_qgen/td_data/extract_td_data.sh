feac57e239e2dc8471d3470ae82e985f5fc73b4f
Orri Erling <oerling@openliniksw.com>
2013-11-14 12:58:25 +0100

    Testing

--- ldbc_socialnet_qgen/td_data/extract_td_data.sh
+++ ldbc_socialnet_qgen/td_data/extract_td_data.sh
@@ -1,1 +1,1 @@
-for f in *.sql; do isql 1204 < ${f%%.*}.sql | head -n -3 | tail -n +9 > ${f%%.*}.txt; done
+for f in *.sql; do isql 1203 < ${f%%.*}.sql | head -n -3 | tail -n +9 > ${f%%.*}.txt; done
