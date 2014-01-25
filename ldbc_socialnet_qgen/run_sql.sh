34851d9950275f3d56e6655cdba247088f773345
Mirko Spasic <mirkospasic@gmail.com>
2014-01-25 16:24:48 +0100

    SQL Driver

---
+++ ldbc_socialnet_qgen/run_sql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+basedir=${0%/*}
+libdir=${basedir}/lib
+SEP=':'
+
+if [ -z "$CLASSPATH" ]
+then
+  CLASSPATH=${basedir}/bin  # for development version only
+else
+  CLASSPATH=$CLASSPATH$SEP${basedir}/bin  # for development version only
+fi    
+
+if [ "$(uname)" = "Cygwin" ]; then SEP=';'; fi
+for jar in $libdir/*.jar
+do
+  if [ ! -e "$jar" ]; then continue; fi
+  CLASSPATH="$CLASSPATH$SEP$jar"
+done
+
+export CLASSPATH
+
+./sibdriver -t 300000 -idir td_data -uc sib/int_sql -mt 1 -runs 1 -sql jdbc:virtuoso://localhost:1205/UID=dba/PWD=dba -dbdriver virtuoso.jdbc4.Driver -printres -pq -defaultparams
