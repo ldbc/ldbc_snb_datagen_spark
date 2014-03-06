fa29f7b77125becbebbe61c45d07e573eb76776c
Arnau <aprat@mint>
2014-03-06 18:07:51 +0100

    Added tool to extract the cumulative degree distribution

---
+++ ldbc_socialnet_dbgen/tools/degreeAnalysis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]
+then
+      echo "Usage:"
+      echo "sh degreeAnalysis.sh <person_knows_person> <outputfile>"
+      exit
+fi
+
+if [ -z $1 ]
+then
+      echo "Missing person_knows_peson file"
+      echo "Usage:"
+      echo "sh degreeAnalysis.sh <person_knows_person> <outputfile>"
+      exit
+fi
+
+if [ -z $2 ]
+then
+      echo "Missing output file"
+      echo "Usage:"
+      echo "sh degreeAnalysis.sh <person_knows_person> <output file>"
+      exit
+fi
+
+rm -f .degreeAnalysis.tmp
+python extractDegrees.py $1 .degreeAnalysis.tmp 
+R --no-save --args .degreeAnalysis.tmp $2 degree < cumulative.R 
+rm .degreeAnalysis.tmp
---
+++ ldbc_socialnet_dbgen/tools/extractDegrees.py
@@ -0,0 +1,39 @@
+
+import sys, os
+import math
+from datetime import datetime, date, time
+import time as _time
+
+knowsFile = open(sys.argv[1],'r')
+
+edgesPerPerson={}
+SEPARATOR="|"
+index=0
+numEdges=0;
+for line in knowsFile.readlines():
+    if index > 0:
+        edge = line.split(SEPARATOR)
+        if int(edge[0]) in edgesPerPerson:
+            edgesPerPerson[int(edge[0])]+=1
+        else:
+            edgesPerPerson[int(edge[0])]=1
+        numEdges+=1
+    index+=1   
+knowsFile.close()
+
+outputFile = open(sys.argv[2],'w')
+histogram = {}
+for person in edgesPerPerson:
+    degree = edgesPerPerson[person]
+    outputFile.write(str(degree)+"\n")
+    #if degree in histogram:
+    #    histogram[degree]+=1
+    #else:
+    #    histogram[degree]=1
+
+outputFile.close()
+    
+
+#print("Average degree: "+str(numEdges/float(len(edgesPerPerson))))
+#for degree in histogram:
+#    outputFile.write(str(degree)+" "+str(histogram[degree])+" \n")
