0373ff0f34133113f288e29c6bf863266f57c6d9
vi <vi@masala.(none)>
2014-04-17 10:19:59 +0200

    Modified weight beetween two persons

--- ldbc_socialnet_qgen/sib/prep.sql
+++ ldbc_socialnet_qgen/sib/prep.sql
@@ -15,11 +15,11 @@ create procedure c_weight (in p1 bigint, in p2 bigint)
      return 0;
   return 0.5 + 
   	  (select count (*) from post ps1, post ps2
-	    where ps1.ps_creatorid = p1 and ps1.ps_replyof = ps2.ps_postid and ps2.ps_creatorid = p2) +
-	     (select count (*) from post ps1, post ps2
-	       where ps1.ps_creatorid = p2 and ps1.ps_replyof = ps2.ps_postid and ps2.ps_creatorid = p1) +
-	        (select 0.5 * count (*) from post c1, post c2
-		 where c1.ps_creatorid = p1 and c1.ps_replyof = c2.ps_postid and c2.ps_creatorid = p2) +
-		  (select 0.5 * count (*) from post c1, post c2
-		   where c1.ps_creatorid = p2 and c1.ps_replyof = c2.ps_postid and c2.ps_creatorid = p1);
+	   where ps1.ps_creatorid = p1 and ps1.ps_replyof = ps2.ps_postid and ps2.ps_creatorid = p2 and ps2.ps_replyof is null) +
+	  (select count (*) from post ps1, post ps2
+	   where ps1.ps_creatorid = p2 and ps1.ps_replyof = ps2.ps_postid and ps2.ps_creatorid = p1 and ps2.ps_replyof is null) +
+	  (select 0.5 * count (*) from post c1, post c2
+	   where c1.ps_creatorid = p1 and c1.ps_replyof = c2.ps_postid and c2.ps_creatorid = p2 and c2.ps_replyof is not null) +
+	  (select 0.5 * count (*) from post c1, post c2
+	   where c1.ps_creatorid = p2 and c1.ps_replyof = c2.ps_postid and c2.ps_creatorid = p1 and c2.ps_replyof is not null);
 }
