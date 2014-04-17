7fcf92fe4f52ba8c618a5abb46bca622bd16967b
vi <vi@masala.(none)>
2014-04-17 10:01:38 +0200

    Added SIB Int Query 14

--- ldbc_socialnet_qgen/run_sql.sh
+++ ldbc_socialnet_qgen/run_sql.sh
@@ -20,4 +20,4 @@ done
 
 export CLASSPATH
 
-./sibdriver -t 300000 -idir td_data -uc sib/int_sql -mt 8 -runs 100 -sql jdbc:virtuoso://localhost:1206/UID=dba/PWD=dba -dbdriver virtuoso.jdbc4.Driver -printres -w 10
+./sibdriver -t 600000 -idir td_data -uc sib/int_sql -mt 1 -runs 15 -sql jdbc:virtuoso://localhost:1207/UID=dba/PWD=dba -dbdriver virtuoso.jdbc4.Driver -printres -w 5
