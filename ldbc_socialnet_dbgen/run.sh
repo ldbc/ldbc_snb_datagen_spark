374abe702165b40e022619bc68a948fa9ca19468
Arnau Prat <aprat@minerva.pc>
2014-05-30 17:03:49 +0200

    pushed run

--- ldbc_socialnet_dbgen/run.sh
+++ ldbc_socialnet_dbgen/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-HADOOP_HOME=/home/user/hadoop-1.2.1 #change to your hadoop folder
-LDBC_SOCIALNET_DBGEN_HOME=/home/user/ldbc_socialnet_bm/ldbc_socialnet_dbgen #change to your ldbc_socialnet_dbgen folder 
-NUM_MACHINES=1 #the number of threads to use.
-OUTPUT_DIR=/home/user #change to the folder where the generated data should be written
+HADOOP_HOME=/home/aprat/hadoop-1.2.1 #change to your hadoop folder
+LDBC_SOCIALNET_DBGEN_HOME=/scratch/aprat/generador/ldbc_socialnet_bm/ldbc_socialnet_dbgen #change to your ldbc_socialnet_dbgen folder 
+NUM_MACHINES=4 #the number of threads to use.
+OUTPUT_DIR=/scratch/aprat/generador #change to the folder where the generated data should be written
 
 export HADOOP_HOME
 export LDBC_SOCIALNET_DBGEN_HOME
@@ -14,8 +14,8 @@ export OUTPUT_DIR
 export HADOOP_TMP_DIR=$OUTPUT_DIR/hadoop
 export DATA_OUTPUT_DIR=$OUTPUT_DIR/social_network
 
-mvn clean
-mvn assembly:assembly
+/usr/local/apache-maven-3.1.0/bin/mvn clean
+/usr/local/apache-maven-3.1.0/bin/mvn assembly:assembly
 
 cp $LDBC_SOCIALNET_DBGEN_HOME/target/ldbc_socialnet_dbgen.jar $LDBC_SOCIALNET_DBGEN_HOME/
 rm $LDBC_SOCIALNET_DBGEN_HOME/target/ldbc_socialnet_dbgen.jar
