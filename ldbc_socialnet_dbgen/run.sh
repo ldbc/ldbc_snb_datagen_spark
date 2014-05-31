66638da95e6fd1e38a6b00ad269ee8eaba5191e6
ArnauPrat <aprat@APratLaptop.(none)>
2014-06-01 00:52:43 +0200

    Fixed run.sh

--- ldbc_socialnet_dbgen/run.sh
+++ ldbc_socialnet_dbgen/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-HADOOP_HOME=/home/aprat/Programs/hadoop-1.2.1 #change to your hadoop folder
-LDBC_SOCIALNET_DBGEN_HOME=/home/aprat/Projects/LDBC/generator/ldbc_socialnet_bm/ldbc_socialnet_dbgen #change to your ldbc_socialnet_dbgen folder 
+HADOOP_HOME=/home/user/hadoop-1.2.1 #change to your hadoop folder
+LDBC_SOCIALNET_DBGEN_HOME=/home/user/ldbc_socialnet_bm/ldbc_socialnet_dbgen #change to your ldbc_socialnet_dbgen folder 
 NUM_MACHINES=1 #the number of threads to use.
-OUTPUT_DIR=/home/aprat/output #change to the folder where the generated data should be written
+OUTPUT_DIR=/home/user/output #change to the folder where the generated data should be written
 
 export HADOOP_HOME
 export LDBC_SOCIALNET_DBGEN_HOME
