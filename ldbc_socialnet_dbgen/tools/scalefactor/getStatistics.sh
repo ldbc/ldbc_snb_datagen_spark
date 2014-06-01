284dbecfe7781ef1f94da0a35cb4fa46366caa5e
minhducit <minhducit@gmail.com>
2014-06-01 21:43:15 +0200

    Update scripts

--- ldbc_socialnet_dbgen/tools/scalefactor/getStatistics.sh
+++ ldbc_socialnet_dbgen/tools/scalefactor/getStatistics.sh
@@ -1,20 +1,21 @@
-RCOMMANT=/usr/bin/R
+#RCOMMAND=/usr/bin/R
+RCOMMAND=R
 #May need to install additional package such as data.table, igraph, bit64
 #install.packages("data.table")
 
-DATAOUTPUTDIR=/export/scratch2/duc/work/LDBC/ldbc_socialnet_bm/ldbc_socialnet_dbgen/datasetFolder/social_network
+DATAOUTPUTDIR=/scratch/duc/ldbc/ldbc_socialnet_bm/ldbc_socialnet_dbgen/datasetFolder/s1/social_network
 
 echo "Number of comments per users"
-$RCOMMANT --slave -f minmaxmean_comment.R --args $DATAOUTPUTDIR/comment_hasCreator_person_*.csv
+$RCOMMAND --slave -f minmaxmean_comment.R --args $DATAOUTPUTDIR/comment_hasCreator_person_*.csv 
 
 echo "Number of posts per users"
-$RCOMMANT --slave -f minmaxmean_post.R --args $DATAOUTPUTDIR/post_hasCreator_person_*.csv
+$RCOMMAND --slave -f minmaxmean_post.R --args $DATAOUTPUTDIR/post_hasCreator_person_*.csv 
 
 echo "Number of friends per users"
-$RCOMMANT --slave -f minmaxmean_friendships.R --args $DATAOUTPUTDIR/person_knows_person_*.csv
+$RCOMMAND --slave -f minmaxmean_friendships.R --args $DATAOUTPUTDIR/person_knows_person_*.csv 
 
 echo "Number of likes per users"
-$RCOMMANT --slave -f minmaxmean_likes.R --args $DATAOUTPUTDIR/*likes*.csv
+$RCOMMAND --slave -f minmaxmean_likes.R --args $DATAOUTPUTDIR/*likes*.csv 
 
 echo "Network cluster coefficient"							       
-R --slave -f transitivity.R --args $DATAOUTPUTDIR/person_knows_person_*.csv
+R --slave -f transitivity.R --args $DATAOUTPUTDIR/person_knows_person_*.csv 
