e5d15d700a76642c36ca4f8a239a2c7b86f9d319
ArnauPrat <aprat@APratLaptop.(none)>
2014-06-05 11:05:58 +0200

    Improved scale_factor extraction scripts

--- ldbc_socialnet_dbgen/tools/scalefactor/extract_sizes.sh
+++ ldbc_socialnet_dbgen/tools/scalefactor/extract_sizes.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DATA_GENERATOR=/scratch/aprat/generador/ldbc_socialnet_bm/ldbc_socialnet_dbgen
-#NUM_USERS="10000 20000 50000 80000 100000 250000 500000"
-NUM_USERS="80000 100000 250000 500000"
-SIZE_STATISTICS_DIR=/home/aprat
-HADOOP_DIR=/home/aprat/hadoop-1.2.1
-OUTPUT_DIR=/scratch/aprat/generador
+NUM_USERS="10000 20000 50000 80000 100000 250000 500000 1000000 3000000"
+DATA_GENERATOR=/path/to/dbgen/
+SIZE_STATISTICS_DIR=/path/to/size_statistics/folder
+HADOOP_DIR=/path/to/hadoop
+OUTPUT_DIR=/path/to/dbgen/output
 
 
 cd $DATA_GENERATOR 
-#rm $SIZE_STATISTICS_DIR/size_statistics.txt
+rm $SIZE_STATISTICS_DIR/size_statistics.txt
+echo "Persons Bytes" >> $SIZE_STATISTICS_DIR/size_statistics.txt
 for num_users in $NUM_USERS
 do 
 	echo "numPersons:$num_users" > $DATA_GENERATOR/params.ini
@@ -17,12 +17,10 @@ do
 	echo "numYears:3" >> $DATA_GENERATOR/params.ini
 	echo "serializerType:csv" >> $DATA_GENERATOR/params.ini
 	echo "enableCompression:false" >> $DATA_GENERATOR/params.ini
+	echo "outputDir:$OUTPUT_DIR" >> $DATA_GENERATOR/params.ini
 	sh run.sh
 	$HADOOP_DIR/bin/hadoop fs -rm $OUTPUT_DIR/social_network/*.json
 	SIZE=$($HADOOP_DIR/bin/hadoop fs -dus $OUTPUT_DIR/social_network | cut -f 2)
 	echo "$num_users  $SIZE" >> $SIZE_STATISTICS_DIR/size_statistics.txt
 done 
-
-
-
 
--- ldbc_socialnet_dbgen/tools/scalefactor/scale_factor_regression.R
+++ ldbc_socialnet_dbgen/tools/scalefactor/scale_factor_regression.R
@@ -4,9 +4,9 @@ print(args[1]);
 table = read.table(paste(args[1],sep=''),header=TRUE)
 names(table)
 m <- lm(formula=log(Persons) ~ log(Bytes),data=table)
+#here is the list of scale factors to retrieve
 scaleFactors <- data.frame(Bytes=c(1073741824, 3221225472, 10737418240, 32212254720, 107374182400, 322122547200, 1099511628000))
 predicted <- exp(predict(m,scaleFactors))
 for( i in 1:length(predicted)) {
     print(paste(round(predicted[[i]],digits=-3),scaleFactors$Bytes[[i]],sep=' '))
-    #print(paste(round(predicted[[i]],digits=0),scaleFactors$Bytes[[i]],sep=' '))
 }
