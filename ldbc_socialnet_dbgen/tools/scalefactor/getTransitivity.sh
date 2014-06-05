10b684a49867ca6361727c257ae617f592645cc9
Pham Minh Duc <duc@bricks05.scilens.private>
2014-06-05 15:36:33 +0200

    Delete unneccessary files

--- ldbc_socialnet_dbgen/tools/scalefactor/getTransitivity.sh
+++
@@ -1,1 +0,0 @@
-R --slave -f transitivity.R --args /scratch/duc/ldbc/ldbc_socialnet_bm/ldbc_socialnet_dbgen/datasetFolder/1MData/person_knows_person_*.csv
--- ldbc_socialnet_dbgen/tools/scalefactor/minmaxmean_post_original.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-library(igraph)
- 
-message("Loading files")
-dflist <- lapply(commandArgs(trailingOnly = TRUE), fread, sep="|", header=T, select=1:2)
-df <- rbindlist(dflist)
-
-d2 <- df[,length(Post.id),by=Person.id]
-message("Min: ",min(d2$V1),", Max: ", max(d2$V1), " Mean: ", mean(d2$V1), " Median: ", median(d2$V1))
