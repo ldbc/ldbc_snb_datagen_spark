e2c64cccc83496b26758c7b25e1cb9826e19d7c7
ArnauPrat <aprat@APratLaptop.(none)>
2014-06-05 10:48:17 +0200

    Added scale factor computing scripts. Updated scale factors.

---
+++ ldbc_socialnet_dbgen/tools/scalefactor/scale_factor_regression.R
@@ -0,0 +1,12 @@
+
+args <- commandArgs(trailingOnly=TRUE);
+print(args[1]);
+table = read.table(paste(args[1],sep=''),header=TRUE)
+names(table)
+m <- lm(formula=log(Persons) ~ log(Bytes),data=table)
+scaleFactors <- data.frame(Bytes=c(1073741824, 3221225472, 10737418240, 32212254720, 107374182400, 322122547200, 1099511628000))
+predicted <- exp(predict(m,scaleFactors))
+for( i in 1:length(predicted)) {
+    print(paste(round(predicted[[i]],digits=-3),scaleFactors$Bytes[[i]],sep=' '))
+    #print(paste(round(predicted[[i]],digits=0),scaleFactors$Bytes[[i]],sep=' '))
+}
