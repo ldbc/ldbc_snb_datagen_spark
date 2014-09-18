7ea8c5a2b56761f17de29937cc330f4056967ce5
Arnau Prat <aprat@ac.upc.edu>
2014-09-18 11:04:40 +0200

    Fixed run.sh script variables

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 HADOOP_HOME=/home/user/hadoop-1.2.1 #change to your hadoop folder
-LDBC_SOCIALNET_DBGEN_HOME=/home/user/ldbc_snb_datagen #change to your ldbc_socialnet_dbgen folder 
+LDBC_SNB_DATAGEN_HOME=/home/user/ldbc_snb_datagen #change to your ldbc_socialnet_dbgen folder 
 
 export HADOOP_HOME
-export LDBC_SOCIALNET_DBGEN_HOME
+export LDBC_SNB_DATAGEN_HOME
 
 mvn clean
 mvn assembly:assembly
 
-cp $LDBC_SOCIALNET_DBGEN_HOME/target/ldbc_socialnet_dbgen.jar $LDBC_SOCIALNET_DBGEN_HOME/
-rm $LDBC_SOCIALNET_DBGEN_HOME/target/ldbc_socialnet_dbgen.jar
+cp $LDBC_SNB_DATAGEN_HOME/target/ldbc_socialnet_dbgen.jar $LDBC_SNB_DATAGEN_HOME/
+rm $LDBC_SNB_DATAGEN_HOME/target/ldbc_socialnet_dbgen.jar
 
-$HADOOP_HOME/bin/hadoop  jar $LDBC_SOCIALNET_DBGEN_HOME/ldbc_socialnet_dbgen.jar $LDBC_SOCIALNET_DBGEN_HOME/params.ini 
+$HADOOP_HOME/bin/hadoop  jar $LDBC_SNB_DATAGEN_HOME/ldbc_socialnet_dbgen.jar $LDBC_SNB_DATAGEN_HOME/params.ini 
 
 #parameter generation
 PARAM_GENERATION=1
@@ -19,7 +19,7 @@ PARAM_GENERATION=1
 if [ $PARAM_GENERATION -eq 1 ]
 then
 	mkdir -p substitution_parameters
-	python paramgenerator/generateparams.py $LDBC_SOCIALNET_DBGEN_HOME substitution_parameters/
+	python paramgenerator/generateparams.py $LDBC_SNB_DATAGEN_HOME substitution_parameters/
   	rm -f m0factors.txt
 	rm -f m0friendList*
 fi
