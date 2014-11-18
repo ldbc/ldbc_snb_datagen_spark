f9568be528f8721cc82078d15036b7723f65c7ce
Arnau Prat <aprat@ac.upc.edu>
2014-11-18 15:40:40 +0100

    .

--- src/main/java/ldbc/snb/datagen/hadoop/HadoopKnowsGenerator.java
+++ src/main/java/ldbc/snb/datagen/hadoop/HadoopKnowsGenerator.java
@@ -29,6 +29,8 @@ public class HadoopKnowsGenerator {
         private Configuration conf;
 
         protected void setup(Context context) {
+            DatagenParams.readConf(conf);
+            DatagenParams.readParameters("/params.ini");
             this.knowsGenerator = new KnowsGenerator();
             this.conf = context.getConfiguration();
         }
