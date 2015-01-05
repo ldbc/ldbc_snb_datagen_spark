b5f4a530eda0e615c59b4709962bff67a4e7af96
Arnau Prat <arnau.prat@gmail.com>
2015-01-05 15:08:23 +0100

    Fixed error with parameter parsing

--- src/main/java/ldbc/snb/datagen/hadoop/HadoopPersonActivityGenerator.java
+++ src/main/java/ldbc/snb/datagen/hadoop/HadoopPersonActivityGenerator.java
@@ -36,19 +36,19 @@ public class HadoopPersonActivityGenerator {
     public static class HadoopPersonActivityGeneratorReducer  extends Reducer<BlockKey, Person, LongWritable, Person> {
 
         private int reducerId;                          /** The id of the reducer.**/
-	private PersonActivitySerializer personActivitySerializer_;
-	private PersonActivityGenerator personActivityGenerator_;
-	private UpdateEventSerializer updateSerializer_;
+        private PersonActivitySerializer personActivitySerializer_;
+        private PersonActivityGenerator personActivityGenerator_;
+        private UpdateEventSerializer updateSerializer_;
 
         protected void setup(Context context) {
             Configuration conf = context.getConfiguration();
             reducerId = context.getTaskAttemptID().getTaskID().getId();
-	    LDBCDatagen.init(conf);
+            LDBCDatagen.init(conf);
             try {
                 personActivitySerializer_ = (PersonActivitySerializer) Class.forName(conf.get("ldbc.snb.datagen.serializer.personActivitySerializer")).newInstance();
-		personActivitySerializer_.initialize(conf,reducerId);
-		updateSerializer_ = new UpdateEventSerializer(conf, DatagenParams.hadoopDir+"/temp_updateStream_forum_"+reducerId, DatagenParams.numPartitions);
-		personActivityGenerator_ = new PersonActivityGenerator(personActivitySerializer_, updateSerializer_);
+                personActivitySerializer_.initialize(conf,reducerId);
+                updateSerializer_ = new UpdateEventSerializer(conf, DatagenParams.hadoopDir+"/temp_updateStream_forum_"+reducerId, DatagenParams.numPartitions);
+                personActivityGenerator_ = new PersonActivityGenerator(personActivitySerializer_, updateSerializer_);
 
             } catch( Exception e ) {
                 System.err.println(e.getMessage());
@@ -56,76 +56,76 @@ public class HadoopPersonActivityGenerator {
         }
 
         @Override
-	public void reduce(BlockKey key, Iterable<Person> valueSet,Context context)
-		throws IOException, InterruptedException {
-		ArrayList<Person> persons = new ArrayList<Person>();
-		for( Person p : valueSet ) {
-			persons.add(new Person(p));
-			
-			for( Knows k : p.knows() ) {
-				if( k.creationDate() > Dictionaries.dates.getUpdateThreshold() && DatagenParams.updateStreams ) {
-					updateSerializer_.export(k);
-				}
-			}
-		}
-		personActivityGenerator_.generateActivityForBlock((int)key.block, persons, context );
+        public void reduce(BlockKey key, Iterable<Person> valueSet,Context context)
+        throws IOException, InterruptedException {
+        ArrayList<Person> persons = new ArrayList<Person>();
+        for( Person p : valueSet ) {
+            persons.add(new Person(p));
 
-		
+            for( Knows k : p.knows() ) {
+                if( k.creationDate() > Dictionaries.dates.getUpdateThreshold() && DatagenParams.updateStreams ) {
+                    updateSerializer_.export(k);
+                }
+            }
+        }
+        personActivityGenerator_.generateActivityForBlock((int)key.block, persons, context );
+
+
         }
         protected void cleanup(Context context){
-		personActivitySerializer_.close();
-		updateSerializer_.close();
+            personActivitySerializer_.close();
+            updateSerializer_.close();
         }
     }
 
 
-	private Configuration conf;
+    private Configuration conf;
 
-	public HadoopPersonActivityGenerator(Configuration conf) {
-		this.conf = conf;
-	}
+    public HadoopPersonActivityGenerator(Configuration conf) {
+        this.conf = conf;
+    }
 
-	public void run( String inputFileName ) throws Exception {
-		
-		FileSystem fs = FileSystem.get(conf);
-		
-		String keyChangedFileName = conf.get("ldbc.snb.datagen.serializer.hadoopDir") + "/key_changed";
-		HadoopFileKeyChanger keyChanger = new HadoopFileKeyChanger(conf, LongWritable.class,Person.class,"ldbc.snb.datagen.hadoop.RandomKeySetter");
-		keyChanger.run(inputFileName,keyChangedFileName);
-		
-		
-		String rankedFileName = conf.get("ldbc.snb.datagen.serializer.hadoopDir") + "/ranked";
-		HadoopFileRanker hadoopFileRanker = new HadoopFileRanker( conf, TupleKey.class, Person.class );
-		hadoopFileRanker.run(keyChangedFileName,rankedFileName);
-		fs.delete(new Path(keyChangedFileName), true);
-		
-		int numThreads = Integer.parseInt(conf.get("ldbc.snb.datagen.generator.numThreads"));
-		Job job = Job.getInstance(conf, "Person Activity Generator/Serializer");
-		job.setMapOutputKeyClass(BlockKey.class);
-		job.setMapOutputValueClass(Person.class);
-		job.setOutputKeyClass(LongWritable.class);
-		job.setOutputValueClass(Person.class);
-		job.setJarByClass(HadoopBlockMapper.class);
-		job.setMapperClass(HadoopBlockMapper.class);
-		job.setReducerClass(HadoopPersonActivityGeneratorReducer.class);
-		job.setNumReduceTasks(numThreads);
-		job.setInputFormatClass(SequenceFileInputFormat.class);
-		job.setOutputFormatClass(SequenceFileOutputFormat.class);
-		
-		job.setSortComparatorClass(BlockKeyComparator.class);
-		job.setGroupingComparatorClass(BlockKeyGroupComparator.class);
-		job.setPartitionerClass(HadoopBlockPartitioner.class);
-		
-		FileInputFormat.setInputPaths(job, new Path(rankedFileName));
-		FileOutputFormat.setOutputPath(job, new Path(conf.get("ldbc.snb.datagen.serializer.hadoopDir")+"/aux"));
-		job.waitForCompletion(true);
-		
-		try{
-			fs.delete(new Path(rankedFileName), true);
-			fs.delete(new Path(conf.get("ldbc.snb.datagen.serializer.hadoopDir")+"/aux"),true);
-		} catch(IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-	
+    public void run( String inputFileName ) throws Exception {
+
+        FileSystem fs = FileSystem.get(conf);
+
+        String keyChangedFileName = conf.get("ldbc.snb.datagen.serializer.hadoopDir") + "/key_changed";
+        HadoopFileKeyChanger keyChanger = new HadoopFileKeyChanger(conf, LongWritable.class,Person.class,"ldbc.snb.datagen.hadoop.RandomKeySetter");
+        keyChanger.run(inputFileName,keyChangedFileName);
+
+
+        String rankedFileName = conf.get("ldbc.snb.datagen.serializer.hadoopDir") + "/ranked";
+        HadoopFileRanker hadoopFileRanker = new HadoopFileRanker( conf, TupleKey.class, Person.class );
+        hadoopFileRanker.run(keyChangedFileName,rankedFileName);
+        fs.delete(new Path(keyChangedFileName), true);
+
+        int numThreads = Integer.parseInt(conf.get("ldbc.snb.datagen.generator.numThreads"));
+        Job job = Job.getInstance(conf, "Person Activity Generator/Serializer");
+        job.setMapOutputKeyClass(BlockKey.class);
+        job.setMapOutputValueClass(Person.class);
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(Person.class);
+        job.setJarByClass(HadoopBlockMapper.class);
+        job.setMapperClass(HadoopBlockMapper.class);
+        job.setReducerClass(HadoopPersonActivityGeneratorReducer.class);
+        job.setNumReduceTasks(numThreads);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+        job.setSortComparatorClass(BlockKeyComparator.class);
+        job.setGroupingComparatorClass(BlockKeyGroupComparator.class);
+        job.setPartitionerClass(HadoopBlockPartitioner.class);
+
+        FileInputFormat.setInputPaths(job, new Path(rankedFileName));
+        FileOutputFormat.setOutputPath(job, new Path(conf.get("ldbc.snb.datagen.serializer.hadoopDir")+"/aux"));
+        job.waitForCompletion(true);
+
+        try{
+            fs.delete(new Path(rankedFileName), true);
+            fs.delete(new Path(conf.get("ldbc.snb.datagen.serializer.hadoopDir")+"/aux"),true);
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
