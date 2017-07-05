835a93434d14198c057be54b4651d62d9ab6ff9d
Arnau  Prat <aprat@ac.upc.edu>
2017-07-05 12:58:25 +0200

    Fixed error with an unhandled exception

--- src/main/java/ldbc/snb/datagen/generator/DatagenParams.java
+++ src/main/java/ldbc/snb/datagen/generator/DatagenParams.java
@@ -226,7 +226,7 @@ public class DatagenParams {
     public static int   numUpdatePartitions         = 1;
 
 
-    public static void readConf( Configuration conf ) throws Exception {
+    public static void readConf( Configuration conf ) {
         try {
 
             ParameterNames values[] = ParameterNames.values();
@@ -324,7 +324,7 @@ public class DatagenParams {
             System.out.println(" ... Start Year " + startYear);
             System.out.println(" ... Num Years " + numYears);
         } catch (Exception e) {
-            throw e;
+            throw new RuntimeException(e);
         }
     }
 }
