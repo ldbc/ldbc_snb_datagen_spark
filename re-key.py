024a6410054827cedcfc46825092be4d7f4afbe5
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-10 12:49:07 -0500

    Working re-keying script

--- re-key.py
+++ re-key.py
@@ -1,86 +1,90 @@
 #!/usr/bin/env python3
 
+## Re-key the dataset, i.e., replace the ids with surrogate (global) ids
+
 import glob
 import re
-from tempfile import NamedTemporaryFile
 
-prefix="social_network/"
-postfix="_0_0.csv"
+def get_id(line, index):
+    return int(line.split('|')[index])
+
+prefix = "social_network/"
+postfix = "_0_0.csv"
 vertex_labels = ["comment", "forum", "organisation", "person", "place", "post", "tagclass", "tag"]
 
-new_id = 0
+global_id = 0
 
 for vertex_label in vertex_labels:
-    print(vertex_label)
+    print("(:" + vertex_label + ")")
 
     filename = prefix + vertex_label + postfix
 
     with open(filename, encoding="utf-8") as file:
-        my_list = file.readlines()
+        lines = file.readlines()
 
-    # trim newlines
-    my_list = [x.strip() for x in my_list]
     # throw away header
-    my_list = my_list[1:]
+    lines = lines[1:]
 
-    old_ids_list = [int(e.split('|')[0]) for e in my_list]
+    old_ids_list = [int(line.split('|')[0]) for line in lines]
     old_ids_set = set(old_ids_list)
 
-    old_new_id_mapping = {}
+    mapping_to_global_id = {}
     for old_id in old_ids_set:
-        old_new_id_mapping[old_id] = new_id
-        new_id = new_id + 1
+        global_id = global_id + 1
+        mapping_to_global_id[old_id] = global_id
 
-    with open(filename, encoding="utf-8") as in_file:
-        csv_data = in_file.read()
+    # re-key vertex file (ID)
+    with open(filename, encoding="utf-8") as file:
+        lines = file.readlines()
 
-    print(old_new_id_mapping)
-    for old_id, new_id in old_new_id_mapping.items():
-        csv_data = csv_data.replace(str(old_id), str(new_id))
+        with open(filename, "w") as out_file:
+            # header
+            out_file.write(lines[0])
+            # content
+            lines = lines[1:]
+            for old_line in lines:
+                old_id = get_id(old_line, 0)
+                new_id = mapping_to_global_id[old_id]
 
-    with open(filename, "w") as out_file:
-        out_file.write(csv_data)
+                # regex to get the first cell with the start id
+                regex = re.compile("^" + str(old_id) + "\|")
+                new_line = regex.sub(str(new_id) + "|", old_line)
+                out_file.write(new_line)
 
 
+    # re-key edge files with the same source vertex label (START_ID)
+    for filename in glob.glob(prefix + vertex_label + "_*" + postfix):
+        print("-> " + filename)
+        with open(filename, encoding="utf-8") as file:
+            lines = file.readlines()
 
-# for vertex_label in ${vertex_labels}; do
-# echo == == == == == == == == == == == == == == == == == == == == == == == == ==
-# echo ${vertex_label}
-# echo == == == == == == == == == == == == == == == == == == == == == == == == ==
-#
-# vertex_file =${vertex_label}$postfix
-# tail - n + 2 ${vertex_file} | cut - d
-# '|' - f
-# 1 | sort - u > current_ids.txt
-#
-# echo
-# vertex
-# type: ${vertex_file}
-# echo
-# outgoing
-# edge
-# types: ${vertex_label}
-# _ *${postfix}
-# echo
-# incoming
-# edge
-# types: *_${vertex_label}${postfix}
-#
-# while read line; do
-# # come up with a new id
-# ((id + +))
-# # replace the old id in the vertex file
-# sed - i
-# "s/$line|/$id|/" ${vertex_file}
-# # replace the old source ids in the edge file
-# for e in ${vertex_label}_ * ${postfix}; do
-# sed - i
-# "s/$line|/$id|/" $e
-# done
-# # replace the old target ids in the egde file
-# for e in ${vertex_label}_ * ${postfix}; do
-# sed - i
-# "s/|$line|/|$id|/" $e
-# done
-# done < current_ids.txt
-# done
+            with open(filename, "w") as out_file:
+                # header
+                out_file.write(lines[0])
+                # content
+                lines = lines[1:]
+                for old_line in lines:
+                    old_id = get_id(old_line, 0)
+                    new_id = mapping_to_global_id[old_id]
+                    regex = re.compile("^" + str(old_id) + "\|")
+                    new_line = regex.sub(str(new_id) + "|", old_line)
+                    out_file.write(new_line)
+
+    # re-key edge files with the same target vertex label (END_ID)
+    for filename in glob.glob(prefix + "*_" + vertex_label + postfix):
+        print("<- " + filename)
+        with open(filename, encoding="utf-8") as file:
+            lines = file.readlines()
+
+            with open(filename, "w") as out_file:
+                # header
+                out_file.write(lines[0])
+                # content
+                lines = lines[1:]
+                for old_line in lines:
+                    old_id = get_id(old_line, 1)
+                    new_id = mapping_to_global_id[old_id]
+                    # regex to get the second cell with the end id
+                    regex = re.compile("^(\d+)\|" + str(old_id))
+                    new_line = regex.sub("\\1|" + str(new_id), old_line)
+                    out_file.write(new_line)
