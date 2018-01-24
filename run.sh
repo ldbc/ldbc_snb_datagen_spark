9a130e43bdf1bacd26375a73a6f8aaf479d813b1
Gabor Szarnyas <szarnyasg@gmail.com>
2018-01-24 23:40:28 +0100

    Use pwd for default SNB home

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEFAULT_HADOOP_HOME=/home/user/hadoop-2.6.0 #change to your hadoop folder
-DEFAULT_LDBC_SNB_DATAGEN_HOME=/home/user/ldbc_snb_datagen #change to your ldbc_socialnet_dbgen folder
+DEFAULT_LDBC_SNB_DATAGEN_HOME=`pwd` #change to your ldbc_snb_datagen folder
 
 # allow overriding configuration from outside via environment variables
 # i.e. you can do
