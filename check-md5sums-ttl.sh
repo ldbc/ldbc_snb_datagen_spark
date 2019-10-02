5abe43eda7ff26605100b9455bb33640d6195a33
Gabor Szarnyas <szarnyasg@gmail.com>
2019-10-02 08:50:02 +0200

    Adjust checksums for Turtle

--- check-md5sums-ttl.sh
+++ check-md5sums-ttl.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-[[ `md5sum social_network/social_network_activity_0_0.ttl | cut -d' ' -f1` == '52a5356ecc757a8e3e5bce2c8ea79557' ]]
-[[ `md5sum social_network/social_network_person_0_0.ttl   | cut -d' ' -f1` == '30973a3dc339617773651b425f7441e4' ]]
+[[ `md5sum social_network/social_network_activity_0_0.ttl | cut -d' ' -f1` == 'a6c697b312ebff2d99e1570e70eb62d4' ]]
+[[ `md5sum social_network/social_network_person_0_0.ttl   | cut -d' ' -f1` == '6ba03dc90924cb7ed24cb69825ea6a83' ]]
 [[ `md5sum social_network/social_network_static_0_0.ttl   | cut -d' ' -f1` == '3c4f4120a2ea1e101cf7d72fbfe30c48' ]]
 
 [[ `md5sum social_network/updateStream_0_0_forum.csv      | cut -d' ' -f1` == '7e00243f68a8171974eabe4ac37df86b' ]]
