3084fc3e210134ef56fafa1432542f151dfad154
jackwaudby <j.waudby2@ncl.ac.uk>
2020-03-19 23:38:03 +0000

    implemented lifespan management for posts in flashmob events.
    Posts during a flashmob event can be created between the maximum of:
    + message creator's forum membership creation date + delta
    And the minimum of:
    + message creator's forum membership deletion date
    + simulation end
    
    Posts during a flashmob event can be deleted between the post creation date plus delta and the minimum of:
    + message creator forum membership deletion date
    + network collapse
    
    Each flashmob event has a specific datetime the event occurs and spans a 72 hours window, flashmobSpan; 36 hours each side of the specific datetime
    
    If the proposed post creation date generated using the homemade distribution used to skew towards the specific event time falls outside the valid creation date interval it is skipped.

--- src/main/java/ldbc/snb/datagen/generator/generators/postgenerators/FlashmobPostGenerator.java
+++ src/main/java/ldbc/snb/datagen/generator/generators/postgenerators/FlashmobPostGenerator.java
@@ -54,6 +54,7 @@ import java.util.TreeSet;
 import static ldbc.snb.datagen.DatagenParams.*;
 
 public class FlashmobPostGenerator extends PostGenerator {
+
     private Distribution dateDistribution;
     private FlashMobTag[] forumFlashmobTags = null;
     private long flashmobSpan;
@@ -62,16 +63,17 @@ public class FlashmobPostGenerator extends PostGenerator {
     public FlashmobPostGenerator(TextGenerator generator, CommentGenerator commentGenerator, LikeGenerator likeGenerator) {
         super(generator, commentGenerator, likeGenerator);
         dateDistribution = new Distribution(DatagenParams.flashmobDistFile);
-        long hoursToMillis_ = 60 * 60 * 1000;
-        flashmobSpan = 72 * hoursToMillis_;
-        dateDistribution.initialize();
+        long hoursToMillis = 60 * 60 * 1000;
+        flashmobSpan = 72 * hoursToMillis; // flash mobs last 3 days
+        dateDistribution.initialize(); // create flashmob distribution
     }
 
     /**
+     * Selects a random tag from a given index.
+     * @param randomFlashmobTag random number generator
+     * @param tags The array of sorted tags to select from.
+     * @param index The first tag to consider.
      * @return The index of a random tag.
-     * @brief Selects a random tag from a given index.
-     * @param[in] tags The array of sorted tags to select from.
-     * @param[in] index The first tag to consider.
      */
     private int selectRandomTag(Random randomFlashmobTag, FlashMobTag[] tags, int index) {
         int upperBound = tags.length - 1;
@@ -90,9 +92,13 @@ public class FlashmobPostGenerator extends PostGenerator {
         return midPoint;
     }
 
+
+
     /**
+     * Selects the earliest flashmob tag index from a given date.
+     * @param tags flashmob tags
+     * @param membership forum members
      * @return The index to the earliest flashmob tag.
-     * @brief Selects the earliest flashmob tag index from a given date.
      */
     private int searchEarliest(FlashMobTag[] tags, ForumMembership membership) {
         long fromDate = membership.getCreationDate() + flashmobSpan / 2 + DatagenParams.deltaTime;
@@ -111,12 +117,18 @@ public class FlashmobPostGenerator extends PostGenerator {
         return midPoint;
     }
 
+    /**
+     *
+     * @param randomNumPost random number generator
+     * @param forum forum
+     */
     private void populateForumFlashmobTags(Random randomNumPost, Forum forum) {
 
-        TreeSet<Integer> tags = new TreeSet<>();
-        List<FlashMobTag> temp = Dictionaries.flashmobs.generateFlashmobTags(randomNumPost, tags, forum
-                .getCreationDate());
+        TreeSet<Integer> tags = new TreeSet<>(); // empty set
+        List<FlashMobTag> temp = Dictionaries.flashmobs.generateFlashmobTags(randomNumPost, tags, forum.getCreationDate());
+
         forumFlashmobTags = new FlashMobTag[temp.size()];
+
         Iterator<FlashMobTag> it = temp.iterator();
         int index = 0;
         int sumLevels = 0;
@@ -135,18 +147,29 @@ public class FlashmobPostGenerator extends PostGenerator {
         }
     }
 
+    /**
+     * Generate flashmob post information
+     * @param randomTag random number generator
+     * @param randomDate random number generator
+     * @param forum forum
+     * @param membership forum member
+     * @return core information for a post during a flashmob
+     */
     protected PostCore generatePostInfo(Random randomTag, Random randomDate, final Forum forum, final ForumMembership membership) {
+
         if (currentForum != forum.getId()) {
             populateForumFlashmobTags(randomTag, forum);
             currentForum = forum.getId();
         }
         if (forumFlashmobTags.length < 1) return null;
+
         PostCore postCore = new PostCore();
+
         int index = searchEarliest(forumFlashmobTags, membership);
         if (index < 0) return null;
         index = selectRandomTag(randomTag, forumFlashmobTags, index);
         FlashMobTag flashmobTag = forumFlashmobTags[index];
-        postCore.getTags().add(flashmobTag.tag);
+        postCore.getTags().add(flashmobTag.tag); // add flashmob tag
 
         for (int i = 0; i < maxNumTagPerFlashmobPost - 1; ++i) {
             if (randomTag.nextDouble() < 0.05) {
@@ -154,8 +177,29 @@ public class FlashmobPostGenerator extends PostGenerator {
                 postCore.getTags().add(tag);
             }
         }
+
+        // add creation date
+        long minCreationDate = membership.getCreationDate() + DatagenParams.deltaTime;
+        long maxCreationDate = Math.min(membership.getDeletionDate(),Dictionaries.dates.getEndDateTime());
+
         double prob = dateDistribution.nextDouble(randomDate);
-        postCore.setCreationDate(flashmobTag.date - flashmobSpan / 2 + (long) (prob * flashmobSpan));
+        long creationDate = flashmobTag.date - flashmobSpan / 2 + (long) (prob * flashmobSpan);
+
+        if (creationDate <= minCreationDate || creationDate >= maxCreationDate) {
+            return null;
+        }
+        postCore.setCreationDate(creationDate);
+
+        // add deletion date
+        long minDeletionDate = creationDate + DatagenParams.deltaTime;
+        long maxDeletionDate = Math.min(membership.getDeletionDate(), Dictionaries.dates.getNetworkCollapse());
+        if (maxDeletionDate - minDeletionDate < 0) {
+            return null;
+        }
+
+        long postDeletionDate = Dictionaries.dates.randomDate(randomDate, minDeletionDate, maxDeletionDate);
+        postCore.setDeletionDate(postDeletionDate);
+
         return postCore;
     }
 }
