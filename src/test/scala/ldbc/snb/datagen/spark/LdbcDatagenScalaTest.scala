95c6eae1494da53731c821281aa2543bd4782b66
Dávid Szakállas <david.szakallas@gmail.com>
2020-04-27 11:47:00 +0200

    add merger and interest test

--- src/test/scala/ldbc/snb/datagen/spark/LdbcDatagenScalaTest.scala
+++ src/test/scala/ldbc/snb/datagen/spark/LdbcDatagenScalaTest.scala
@@ -1,11 +1,10 @@
 package ldbc.snb.datagen.spark
 
-import java.io.{PrintStream, PrintWriter}
 import java.{lang, util}
 import java.util.Properties
 
 import ldbc.snb.datagen._
-import ldbc.snb.datagen.entities.dynamic.person.{IP, Person, PersonSummary}
+import ldbc.snb.datagen.entities.dynamic.person.Person
 import ldbc.snb.datagen.hadoop.HadoopConfiguration
 import ldbc.snb.datagen.hadoop.generator.{HadoopKnowsGenerator, HadoopPersonGenerator}
 import ldbc.snb.datagen.hadoop.key.TupleKey
@@ -13,11 +12,11 @@ import ldbc.snb.datagen.spark.generators.{SparkKnowsGenerator, SparkPersonGenera
 import ldbc.snb.datagen.util.{ConfigParser, LdbcConfiguration}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.mapred.SequenceFileInputFormat
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
 import scala.collection.JavaConverters._
+import scala.reflect.ClassTag
 
 class LdbcDatagenScalaTest extends FunSuite with BeforeAndAfterAll with Matchers {
 
@@ -75,17 +74,36 @@ class LdbcDatagenScalaTest extends FunSuite with BeforeAndAfterAll with Matchers
     actuals shouldBe expecteds
   }
 
-  test("Knows generator returns expected results") {
+  import Keys._
+
+  test("University Knows generator returns expected results (interest)") {
+    shouldGenerateSameKnows(
+      "/knows_university",
+      0,
+      (p: Person) => p.byUni,
+      "ldbc.snb.datagen.hadoop.miscjob.keychanger.UniversityKeySetter"
+    )
+  }
+
+  test("Interest Knows generator returns expected results (interest)") {
+    shouldGenerateSameKnows(
+      "/knows_interest",
+      1,
+      (p: Person) => p.byInterest,
+      "ldbc.snb.datagen.hadoop.miscjob.keychanger.InterestKeySetter"
+    )
+  }
+
+  def shouldGenerateSameKnows[K: ClassTag: Ordering](hadoopDir: String, stepIndex: Int, sparkSorter: Person => K, hadoopSorter: String) = {
     val personsFixturePath = getClass.getResource("/fixtures/hadoop/persons").toString
 
     val knowsGeneratorClassName = "ldbc.snb.datagen.generator.generators.knowsgenerators.DistanceKnowsGenerator"
-    val preKeyHadoop = "ldbc.snb.datagen.hadoop.miscjob.keychanger.UniversityKeySetter"
+    val preKeyHadoop = hadoopSorter
     val postKeySetter = "ldbc.snb.datagen.hadoop.miscjob.keychanger.RandomKeySetter"
-    val stepIndex = 0
 
     val percentages = Seq(0.45f, 0.45f, 0.1f)
     timed(
-      "hadoop knows generation",
+      s"hadoop knows generation for $hadoopDir",
       new HadoopKnowsGenerator(
         hadoopConf,
         preKeyHadoop,
@@ -93,22 +111,20 @@ class LdbcDatagenScalaTest extends FunSuite with BeforeAndAfterAll with Matchers
         percentages.map(new lang.Float(_)).asJava,
         stepIndex,
         knowsGeneratorClassName
-      ).run(personsFixturePath, hadoopPrefix + "/knows")
+      ).run(personsFixturePath, hadoopPrefix + hadoopDir)
     )
 
     val persons = spark.sparkContext
       .hadoopFile[TupleKey, Person, SequenceFileInputFormat[TupleKey, Person]](personsFixturePath)
       .values
 
-    import Keys._
-
     implicit val sparkSession = spark
 
     val actual = SparkKnowsGenerator(persons, conf, percentages, stepIndex,
-      sortBy = _.byUni, knowsGeneratorClassName)
+      sortBy = sparkSorter, knowsGeneratorClassName)
 
     val expected = spark.sparkContext
-      .hadoopFile[TupleKey, Person, SequenceFileInputFormat[TupleKey, Person]](hadoopPrefix + "/knows")
+      .hadoopFile[TupleKey, Person, SequenceFileInputFormat[TupleKey, Person]](hadoopPrefix + hadoopDir)
 
     val expecteds = expected.map { case (_, p) => p.hashCode() }.collect()
     val actuals = actual.map(_.hashCode()).collect()
