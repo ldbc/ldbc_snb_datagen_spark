6b0c384bd3924591cd214758e9f7bf44ebd610af
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-13 21:12:09 +0200

    Move scripts to tools, add documentation

--- tools/run.sh
+++ tools/run.sh
@@ -1,55 +1,6 @@
 #!/bin/bash
 
-if [ ! -f params.ini ]; then
-  echo "Parameters file (params.ini) not found."
-  exit 1
-fi
+docker run -v `pwd`/out:/mnt/data -v `pwd`/params.ini:/mnt/params.ini -v `pwd`/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar:/mnt/datagen.jar ldbc/spark
 
-DEFAULT_HADOOP_HOME=/home/user/hadoop-3.2.1 #change to your hadoop folder
-DEFAULT_LDBC_SNB_DATAGEN_HOME=`pwd` #change to your ldbc_snb_datagen folder
-
-# allow overriding configuration from outside via environment variables
-# i.e. you can do
-#     HADOOP_HOME=/foo/bar LDBC_SNB_DATAGEN_HOME=/baz/quux ./run.sh
-# instead of changing the contents of this file
-HADOOP_HOME=${HADOOP_HOME:-$DEFAULT_HADOOP_HOME}
-LDBC_SNB_DATAGEN_HOME=${LDBC_SNB_DATAGEN_HOME:-$DEFAULT_LDBC_SNB_DATAGEN_HOME}
-
-export HADOOP_HOME
-export LDBC_SNB_DATAGEN_HOME
-
-echo ===============================================================================
-echo Running generator with the following parameters:
-echo -------------------------------------------------------------------------------
-echo LDBC_SNB_DATAGEN_HOME: $LDBC_SNB_DATAGEN_HOME
-echo JAVA_HOME: $JAVA_HOME
-echo HADOOP_HOME: $HADOOP_HOME
-echo HADOOP_CLIENT_OPTS: $HADOOP_CLIENT_OPTS
-echo ===============================================================================
-
-if [ ! -d $LDBC_SNB_DATAGEN_HOME ]; then
-  echo LDBC_SNB_DATAGEN_HOME $LDBC_SNB_DATAGEN_HOME does not exist
-  exit 1
-fi
-
-if [ ! -d $JAVA_HOME ]; then
-  echo JAVA_HOME $JAVA_HOME does not exist
-  exit 1
-fi
-
-if [ ! -d $HADOOP_HOME ]; then
-  echo HADOOP_HOME $HADOOP_HOME does not exist
-  exit 1
-fi
-
-mvn clean
-mvn -DskipTests assembly:assembly
-
-$HADOOP_HOME/bin/hadoop jar $LDBC_SNB_DATAGEN_HOME/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar $LDBC_SNB_DATAGEN_HOME/params.ini
-
-rm -f m*personFactors*
-rm -f .m*personFactors*
-rm -f m*activityFactors*
-rm -f .m*activityFactors*
-rm -f m0friendList*
-rm -f .m0friendList*
+# see issue #178
+sudo chown -R `id -u`:`id -g` out
