beb45f8bf85ae8edd1397b5eabffdb387498c8c5
jackwaudby <j.waudby2@ncl.ac.uk>
2020-10-15 02:08:52 +0100

    some hacking in R

--- tuning/analysis.R
+++ tuning/analysis.R
@@ -2,39 +2,49 @@
 library(tidyverse)
 library(lubridate)
 
-# insert operation type mapping
-# 1 - PERSON
-# 2 - LIKE_POST
-# 3 - LIKE_COMMENT
-# 4 - FORUM
-# 6 - POST
-# 7 - COMMENT
-# 8 - FRIENDSHIP
-# TODO: delete operation type mapping
 
 #### load data ####
-dg_home = Sys.getenv("LDBC_SNB_DATAGEN_HOME")
-person = paste0(dg_home,"/social_network/updateStream_0_0_person_trimmed.csv")
-activity = paste0(dg_home,"/social_network/updateStream_0_0_forum_trimmed.csv")
-person = read_delim(person,"|",col_names = c("timestamp","type"))
-activity = read_delim(activity,"|",col_names = c("timestamp","type"))
-# TODO: load all csv files 
+ldbc = paste0(Sys.getenv("LDBC_SNB_DATAGEN_HOME"),"/out/social_network/dynamic/")
+ldbc = "/Users/jackwaudby/Documents/ldbc/ldbc_snb_datagen/out/social_network/dynamic/"
+post = paste0(ldbc,"post_0_0_trimmed.csv")
+post = read_delim(post,"|")
 
 #### preprocess ####
-person = person %>% mutate(timestamp = as_datetime(timestamp/1000))
-activity = activity %>% mutate(timestamp = as_datetime(timestamp/1000))
-updates = bind_rows(person, activity)
 types = c("PERSON","LIKE_POST","LIKE_COMMENT","FORUM","FORUM_MEMBERSHIP","POST","COMMENT","FRIENDSHIP")
 types = tolower(types)
-# TODO: sort by timestamp and group in batches  
+
 
 #### analysis ####
-operations_per_type = updates %>% count(type) %>% mutate(type = types)
-operations_per_day = updates %>% mutate(timestamp = date(timestamp)) %>% group_by(type) %>% count(timestamp) %>% do({
-   p <- ggplot(., aes(x = timestamp, y = n)) + geom_line()
-   ggsave(p, filename = paste0("./figs/ops_per_day_type_", unique(.$type),  ".pdf"), width = 4, height = 4, units = "in")
-   invisible(.)
-   })
+ss = 1262304000
+se = 1356998400
+bl = as_datetime(ss + ((se-ss)*0.9))
+new_bl = as_datetime(se) - days(7*16) # possible better bulk load date
+weeks = (as_datetime(se) - new_bl) / as.duration(weeks(1))
+
+## inserts 
+post %>% filter(creationDate > new_bl) %>% count() 
+insert_posts_per_day = post %>% filter(creationDate > new_bl) %>% mutate(creationDate = date(creationDate)) %>% count(creationDate)
+p_line <- ggplot(insert_posts_per_day , aes(x = creationDate, y=n)) + geom_line() 
+
+## deletes 
+# number of explicit deletes across simulation period
+post %>% count(explicitlyDeleted) 
+# number of explicit deletes after bulk load threshold
+post %>% filter(explicitlyDeleted == T, deletionDate > new_bl)
+# number of explicit deletes after bulk load threshold per day
+posts_per_day = post %>% filter(explicitlyDeleted == T, deletionDate > new_bl) %>% mutate(deletionDate = date(deletionDate)) %>% count(deletionDate)
+
+p_line <- ggplot(posts_per_day , aes(x = deletionDate, y=n)) + geom_line()
+
+(p_hist <- ggplot(posts_per_day, aes(x=n)) + geom_histogram())
+
+
+# operations_per_type = updates %>% count(type) %>% mutate(type = types)
+# operations_per_day = updates %>% mutate(timestamp = date(timestamp)) %>% group_by(type) %>% count(timestamp) %>% do({
+#    p <- ggplot(., aes(x = timestamp, y = n)) + geom_line()
+#    ggsave(p, filename = paste0("./figs/ops_per_day_type_", unique(.$type),  ".pdf"), width = 4, height = 4, units = "in")
+#    invisible(.)
+#    })
 # TODO: add labels and some colour to plots
 # TODO: operation counts per various batch sizes 
 # TODO: ratio of inserts to deletes in each batch 
--- tuning/run-analysis.sh
+++ tuning/run-analysis.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-# keep operation timestamp and type 
-# TODO: trim arbitrary number of updateStream_*_*_*.csv files
-base=$LDBC_SNB_DATAGEN_HOME/social_network/updateStream_0_0_person.csv
-trimmed=$LDBC_SNB_DATAGEN_HOME/social_network/updateStream_0_0_person_trimmed.csv
-cut -d'|' -f1,3 $base >> $trimmed
+DIR=$LDBC_SNB_DATAGEN_HOME/out/social_network/dynamic/
 
-# run analysis 
-Rscript analysis.R
+person=$DIR/person_0_0.csv
+person_trimmed=$DIR/person_0_0_trimmed.csv
+forum=$DIR/forum_0_0.csv
+forum_trimmed=$DIR/forum_0_0_trimmed.csv
+post=$DIR/post_0_0.csv
+post_trimmed=$DIR/post_0_0_trimmed.csv
+comment=$DIR/comment_0_0.csv
+comment_trimmed=$DIR/comment_0_0_trimmed.csv
+person_likes_comment=$DIR/person_likes_comment_0_0.csv
+person_likes_comment_trimmed=$DIR/person_likes_comment_0_0_trimmed.csv
+person_likes_post=$DIR/person_likes_post_0_0.csv
+person_likes_post_trimmed=$DIR/person_likes_post_0_0_trimmed.csv
+person_knows_person=$DIR/person_knows_person_0_0.csv
+person_knows_person_trimmed=$DIR/person_knows_person_0_0_trimmed.csv
+forum_hasMember_person=$DIR/forum_hasMember_person_0_0.csv
+forum_hasMember_person_trimmed=$DIR/forum_hasMember_person_0_0_trimmed.csv
+
+
+if [ "$1" == "t" ]; then 
+  # keep creationDate, deletionDate, explicitlyDeleted
+  cut -d'|' -f1-3 $person >> $person >> $person_trimmed
+  cut -d'|' -f1-3 $forum >> $forum_trimmed
+  cut -d'|' -f1-3 $post >> $post_trimmed
+  cut -d'|' -f1-3 $comment >> $comment_trimmed
+  cut -d'|' -f1-3 $person_likes_comment >> $person_likes_comment_trimmed
+  cut -d'|' -f1-3 $person_likes_post >> $person_likes_post_trimmed
+  cut -d'|' -f1-3 $person_knows_person >> $person_knows_person
+  cut -d'|' -f1-3 $forum_hasMember_person >> $forum_hasMember_person_trimmed
+else
+  echo "skip trimming"
+fi
+
+if [ "$2" == "a" ]; then 
+  # run analysis 
+  Rscript analysis.R
+else
+  echo "skip analysis"
+fi
