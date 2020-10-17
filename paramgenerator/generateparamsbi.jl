1a89187c7abf0dd27d1d27134b4024ffb6f0ba94
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-17 16:05:53 +0200

    Cleanup WIP Julia code

--- paramgenerator/generateparamsbi.jl
+++ paramgenerator/generateparamsbi.jl
@@ -1,97 +1,5 @@
-# class ParamsWriter:
-#    def __init__(self, outdir, number, param_nameFactors):
-#       self.file = codecs.open(outdir+"/bi_"+str(number)+"_param.txt", "w",encoding="utf-8")
-#       for i in range(0,len(param_nameFactors)):
-#          if i>0:
-#             self.file.write("|")
-#          self.file.write(param_nameFactors[i])
-#       self.file.write("\n")
-
-#    def append(self, params):
-#       for i, param in enumerate(params):
-#          if i>0:
-#             self.file.write("|")
-#          self.file.write(param)
-#       self.file.write("\n")
-
-# def key_params(sample, lower_bound, upper_bound):
-#    results = []
-#    for key, count in sample:
-#       if count > lower_bound and count < upper_bound:
-#          results.append([key, count])
-#    return results
-
-# def serialize_q6(outdir, tagFactors):
-#    writer = ParamsWriter(outdir, 6, ["tag"])
-#    for tag, count in tagFactors:
-#       writer.append([tag])
-
-#    # read precomputed counts from files
-#    (personFactors, countryFactors, tagFactors, tagClassFactors, nameFactors, givennameFactors,  ts, postsHisto) = \
-#       readfactors.load(personFactorFiles,activityFactorFiles, friendsFiles)
-
-#    tag_posts = tagFactors
-#    tag_posts.sort(key=lambda x: x[1], reverse=True)
-
-#    total_posts = 0
-#    for day, count in tag_posts:
-#       total_posts += count
-
-#    serialize_q6 (outdir, key_params(tag_posts, total_posts/1300, total_posts/900))
-
-
-## readfactors.py
-
-
-# for inputFileName in activityFactorFiles:
-#     with codecs.open(inputFileName, "r", "utf-8") as f:
-#         countryCount = int(f.readline())
-#         for i in range(countryCount):
-#             line = f.readline().split(",")
-#             country = line[0]
-#             if not countryFactors.existParam(country):
-#                 countryFactors.addNewParam(country)
-#             countryFactors.addValue(country, "p", int(line[1]))
-
-#         tagClassCount = int(f.readline())
-#         for i in range(tagClassCount):
-#             line = f.readline().split(",")
-#             tagClass = line[0]
-#             if not tagClass in tagClassFactors:
-#                 tagClassFactors[tagClass] = 0
-#             tagClassFactors[tagClass] += int(line[2])
-
-#         tagCount = int(f.readline())
-#         for i in range(tagCount):
-#             line = f.readline()
-#             count = line[1+line.rfind(","):]
-#             name = line[:line.rfind(",")]
-#             if not name in tagFactors:
-#                 tagFactors[name] = 0
-#             tagFactors[name] += int(count)
-
-#         nameCount = int(f.readline())
-#         for i in range(nameCount):
-#             line = f.readline().split(",")
-#             name = line[0]
-#             if not name in nameFactors:
-#                 nameFactors[name] = 0
-#             nameFactors[name] += int(line[1])
-
-#         for i in range(4):
-#             t = f.readline().rstrip()
-#             if timestamp[i] == 0 and t != 'null':
-#                 timestamp[i] = int(t)
-
-# loadFriends(friendFiles, results)
-
-# return (results, countryFactors, tagFactors.items(), tagClassFactors.items(), nameFactors.items(), givennameFactors,timestamp, postsHisto)
-
-
-
-##############################################################################
-# Julia code starts from here
-##############################################################################
+# DefaultDict comes from DataStructures #import Pkg; Pkg.add("DataStructures")
+using DataStructures
 
 # if length(ARGS) < 2
 #     println("arguments: <input dir> <output dir>")
@@ -113,10 +21,6 @@ activityFactorFiles = filter(f -> endswith(f, "activityFactors.txt"), files)
 println(activityFactorFiles)
 #println(personFactorFiles)
 #println(friendsFiles)
-
-# DefaultDict
-#import Pkg; Pkg.add("DataStructures")
-using DataStructures
 
 countryFactors = Dict{String,Int64}()
 tagClassFactors = DefaultDict{String,Int64}(0)
