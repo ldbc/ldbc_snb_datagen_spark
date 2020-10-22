7d40478ea2246a938912280701f97a1cca0cd736
jackwaudby <j.waudby2@ncl.ac.uk>
2020-10-22 03:56:05 +0100

    op counts per batch for posts

--- tuning/analysis.R
+++ tuning/analysis.R
@@ -13,28 +13,45 @@ post = read_delim(post,"|")
 types = c("PERSON","LIKE_POST","LIKE_COMMENT","FORUM","FORUM_MEMBERSHIP","POST","COMMENT","FRIENDSHIP")
 types = tolower(types)
 
-
 #### analysis ####
-ss = 1262304000
-se = 1356998400
-bl = as_datetime(ss + ((se-ss)*0.9))
-new_bl = as_datetime(se) - days(7*16) # possible better bulk load date
-weeks = (as_datetime(se) - new_bl) / as.duration(weeks(1))
+ss = 1262304000 # simulation start 
+se = 1356998400 # simulation end 
+bl = as_datetime(ss + ((se-ss)*0.9)) # current bulk load threshold
+new_bl =  as_datetime("2012-09-04") # proposed bulk load date threshold
+n_batches = (as_datetime(se) - new_bl) / as.duration(weeks(1)) # 17 week-sized batches 
+new_pc = (se-1346716800)/(se-ss) # new bulk load percentage
 
-## inserts 
-post %>% filter(creationDate > new_bl) %>% count() 
-insert_posts_per_day = post %>% filter(creationDate > new_bl) %>% mutate(creationDate = date(creationDate)) %>% count(creationDate)
-p_line <- ggplot(insert_posts_per_day , aes(x = creationDate, y=n)) + geom_line() 
+n_inserts = post %>% filter(creationDate > new_bl) %>% count() # no. inserts: 29094
+n_deletes = post %>% filter(deletionDate > new_bl) %>% filter(explicitlyDeleted == T) %>% count() # no. deletes: 261
 
-## deletes 
-# number of explicit deletes across simulation period
-post %>% count(explicitlyDeleted) 
-# number of explicit deletes after bulk load threshold
-post %>% filter(explicitlyDeleted == T, deletionDate > new_bl)
-# number of explicit deletes after bulk load threshold per day
-posts_per_day = post %>% filter(explicitlyDeleted == T, deletionDate > new_bl) %>% mutate(deletionDate = date(deletionDate)) %>% count(deletionDate)
+n_insert_delete = post %>% filter(creationDate > new_bl) %>% filter(explicitlyDeleted == T) %>% count() # no. inserted and deleted: 118
+n_insert_not_delete = post %>% filter(creationDate > new_bl) %>% filter(explicitlyDeleted == F) %>% count() # no. inserted only: 28976
+n_not_insert_delete = post %>% filter(creationDate <= new_bl && deletionDate > new_bl) %>% filter(explicitlyDeleted == T) %>% count()  # no. deleted only: 0
 
-p_line <- ggplot(posts_per_day , aes(x = deletionDate, y=n)) + geom_line()
+# operation count per batch 
+op_types = c("Insert", "Delete")
+i = post %>% filter(creationDate > new_bl) %>% select(creationDate) %>% rename(timestamp = creationDate) %>% add_column(op_type="Insert") %>% mutate(op_type = factor(op_type,levels = op_types)) 
+d = post %>% filter(deletionDate > new_bl) %>% filter(explicitlyDeleted == T) %>% select(deletionDate) %>% rename(timestamp = deletionDate) %>% add_column(op_type="Delete") %>% mutate(op_type = factor(op_type,levels = op_types))
+o = bind_rows(i,d) %>% arrange(timestamp)
+ 
+rb_i = rep(0,17)
+rb_d = rep(0,17)
+for (i in 1:17) {
+  rb = o %>% filter(timestamp >= new_bl + as.duration(weeks(i-1)) & timestamp < new_bl + as.duration(weeks(i)))
+  total = as.numeric(rb %>% count())
+  rb_i[i]  = as.numeric(rb %>% filter(op_type == "Insert") %>% count())
+  rb_d[i] = total - rb_i[i]
+}
+
+rb_i/(rb_i + rb_d)
+
+
+# plots
+new_posts_per_day = post %>% filter(creationDate > new_bl) %>% mutate(creationDate = date(creationDate)) %>% count(creationDate)
+deleted_posts_per_day = post %>% filter(explicitlyDeleted == T, deletionDate > new_bl) %>% mutate(deletionDate = date(deletionDate)) %>% count(deletionDate)
+
+ggplot(new_posts_per_day , aes(x = creationDate, y=n)) + geom_line() 
+ggplot(deleted_posts_per_day , aes(x = deletionDate, y=n)) + geom_line()
 
 (p_hist <- ggplot(posts_per_day, aes(x=n)) + geom_histogram())
 
@@ -46,5 +63,3 @@ p_line <- ggplot(posts_per_day , aes(x = deletionDate, y=n)) + geom_line()
 #    invisible(.)
 #    })
 # TODO: add labels and some colour to plots
-# TODO: operation counts per various batch sizes 
-# TODO: ratio of inserts to deletes in each batch 
