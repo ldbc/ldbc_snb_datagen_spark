713cc1845c019b36a765f0614706268f045ee573
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-23 18:23:10 +0200

    Cleanup before generating new data

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# clean previously generated data
+rm -rf out/*
 docker run -v `pwd`/out:/mnt/data -v `pwd`/params.ini:/mnt/params.ini -v `pwd`/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar:/mnt/datagen.jar ldbc/spark
 
 # see issue #178
