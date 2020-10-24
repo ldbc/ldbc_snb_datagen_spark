67a0ab1b4a53f238973995e9377b73d01653c640
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-24 14:25:40 +0200

    First draft of Julia BI paramgen

--- paramgenerator/generateparamsbi.jl
+++ paramgenerator/generateparamsbi.jl
@@ -194,15 +194,15 @@ end
 
 function post_month_params(sample, lower_bound, upper_bound)
     results = []
-    for ix in 0:length(sample)÷4-1
-        start_ix = ix*4+1
-        end_ix = min(ix*4+4, length(sample))
+    for ix in 0:length(sample)÷4 - 1
+        start_ix = ix*4 + 1
+        end_ix = min(ix*4 + 4, length(sample))
         count_sum = 0
         for v in sample[start_ix:end_ix]
             count_sum += v[2]
         end
         start_day = sample[start_ix][1]
-        end_day = sample[end_ix][1]
+        end_day = sample[min(end_ix + 1, length(sample))][1] # end_ix + 1 to get the first day of the next month
         if lower_bound < count_sum < upper_bound
             push!(results, [[start_day, end_day], count_sum])
         end
@@ -212,7 +212,7 @@ end
 
 week_posts = convert_posts_histo(postsHisto, timestamps)
 post_months = post_month_params(week_posts, post_lower_threshold, post_upper_threshold)
-non_empty_weeks = length(filter(x -> x[4] != 0, week_posts))
+non_empty_weeks = length(filter(x -> x[2] != 0, week_posts))
 
 persons = personFactors[:, :person]
 shuffle!(MersenneTwister(1234), persons)
@@ -251,13 +251,13 @@ function key_params(sample, lower_bound, upper_bound)
     filter(e -> (lower_bound < e[2] && e[2] < upper_bound), sample)
 end
 
-# Q1
+# Q1 // 1
 post_date_right_open_range_params(week_posts, 0.3*total_posts, 0.6*total_posts)
 
-# Q3
+# Q3 // 2
 bi3 = post_months
 
-# Q4
+# Q4 // 3
 bi4 = []
 for i in key_params(tagclass_posts, total_posts÷20, total_posts÷10)
     for j in key_params(country_sample, total_posts÷150, total_posts÷50)
@@ -266,35 +266,64 @@ for i in key_params(tagclass_posts, total_posts÷20, total_posts÷10)
 end
 bi4
 
-# Q5
+# Q5 // 4
 bi5 = key_params(country_sample, total_posts÷200, total_posts÷100)
 
-# Q6
+# Q6 // 5
 bi6 = key_params(tag_posts, total_posts÷1300, total_posts÷900)
 
-# Q7
+# Q7 // 6
 bi7 = key_params(tag_posts, total_posts÷900, total_posts÷600)
 
-# Q8
+# Q8 // 7
 bi8 = key_params(tag_posts, total_posts÷600, total_posts÷300)
 
-# Q14
+# Q10 // 8
+bi10 = []
+for i in key_params(tag_posts, total_posts÷900, total_posts÷600)
+    for j in post_date_right_open_range_params(week_posts, 0.3*total_posts, 0.6*total_posts)
+        push!(bi10, [i[1], j[1]])
+    end
+end
+bi10
 
-# Q16
+# Q14 // 9
+post_months
 
-# Q17
+# Q16 // 10
+bi16 = []
+r16 = MersenneTwister(1234)
+for i in key_params(tagclass_posts, total_posts÷30, total_posts÷10)
+    for j in key_params(country_sample, total_posts÷80, total_posts÷20)
+        person = rand(r16, persons)
+        push!(bi16, [i[1], j[1], person])
+        # TODO: add bounds
+    end
+end
+bi16
+
+# Q17 // 11
 bi17 = key_params(country_sample, total_posts÷200, total_posts÷100)
 
-# Q18
+# Q18 // 12
+bi18 = []
+for week in post_date_right_open_range_params(week_posts, 0.3*total_posts, 0.6*total_posts)
+    for length in post_lengths
+        for langs in language_codes
+            push!(bi18, [week, length, langs])
+        end
+    end
+end
+bi18
 
-# Q21
+# Q21 // 13
 bi21 = []
 for c in keys(key_params(country_sample, total_posts÷200, total_posts÷100))
     push!(bi21, [c, 1356994800000])
 end
 bi21
 
-# Q22
+# Q22 // 14
 bi22 = []
 for i in keys(key_params(country_sample, total_posts÷120, total_posts÷40))
     for j in keys(key_params(country_sample, total_posts÷120, total_posts÷40))
@@ -305,4 +334,14 @@ for i in keys(key_params(country_sample, total_posts÷120, total_posts÷40))
 end
 bi22
 
-# Q25
+# Q25 // 15
+bi25 = []
+r25 = MersenneTwister(1234)
+for day_range in post_months
+    for i in 1:min(length(persons), 10)
+        person1Id = rand(r25, persons)
+        person2Id = rand(r25, filter(p -> p != person1Id, persons))
+        push!(bi25, [person1Id, person2Id, day_range[1], day_range[2]])
+    end
+end
+bi25
