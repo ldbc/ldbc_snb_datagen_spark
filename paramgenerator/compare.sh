85ce765c95a63fc5a009546e95d7f8b4f43b1488
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-24 14:28:56 +0200

    Fix comparison order

--- paramgenerator/compare.sh
+++ paramgenerator/compare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./regen-python.sh && cd ../substitution_parameters && for i in *.txt; do echo $i: ; diff -u $i ../substitution_parameters_old/$i;  done && cd ../paramgenerator/
+./regen-python.sh && cd ../substitution_parameters && for i in *.txt; do echo $i: ; diff -u ../substitution_parameters_old/$i $i;  done && cd ../paramgenerator/
