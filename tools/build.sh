c3675d04654532d0fe32a9c22200c5ea90b416aa
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-27 23:52:12 +0100

    Remove SELinux-specific code

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# required for SELinux to allow Docker to work
-# we're prompting the user here (for password) so that the run script can proceed once the build is completed
-sudo chcon -Rt svirt_sandbox_file_t .
-
 mvn assembly:assembly -DskipTests
