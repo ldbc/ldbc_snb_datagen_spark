ac22d352f918544eaa64b093608944af438cd481
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-17 11:12:53 +0100

    Check in run script whether necessary files exist

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+[ ! -f params.ini ] && echo "params.ini does not exist, exiting" && exit 1
+[ ! -f target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar ] && echo "target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar does not exist, exiting" && exit 1
+[ ! -d out ] && echo "out/ directory does not exist, exiting" && exit 1
+
 # clean previously generated data
 rm -rf out/*
 docker run -v `pwd`/out:/mnt/data -v `pwd`/params.ini:/mnt/params.ini -v `pwd`/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar:/mnt/datagen.jar ldbc/spark
