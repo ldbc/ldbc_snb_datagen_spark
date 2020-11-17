8979c8ee9982d451cd802468cf2dc13b9a28f086
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-17 12:23:41 +0100

    Drop unnecessary check

--- tools/run.sh
+++ tools/run.sh
@@ -2,7 +2,6 @@
 
 [ ! -f params.ini ] && echo "params.ini does not exist, exiting" && exit 1
 [ ! -f target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar ] && echo "target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar does not exist, exiting" && exit 1
-[ ! -d out ] && echo "out/ directory does not exist, exiting" && exit 1
 
 # clean previously generated data
 rm -rf out/*
