42a082cd3af094cf5658512da2d4208f498d2d32
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-23 23:36:49 +0100

    User non-root user to write to mounted Docker volume. Fixes #178

--- tools/run.sh
+++ tools/run.sh
@@ -3,9 +3,7 @@
 [ ! -f params.ini ] && echo "params.ini does not exist, exiting" && exit 1
 [ ! -f target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar ] && echo "target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar does not exist, exiting" && exit 1
 
-# clean previously generated data
+# make sure that out directory exists and clean previously generated data
+mkdir -p out/
 rm -rf out/*
-docker run -v `pwd`/out:/mnt/data -v `pwd`/params.ini:/mnt/params.ini -v `pwd`/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar:/mnt/datagen.jar ldbc/spark
-
-# see issue #178
-sudo chown -R `id -u`:`id -g` out
+docker run -e uid=`id -u` -v `pwd`/out:/mnt/data -v `pwd`/params.ini:/mnt/params.ini -v `pwd`/target/ldbc_snb_datagen-0.4.0-SNAPSHOT-jar-with-dependencies.jar:/mnt/datagen.jar ldbc/spark
