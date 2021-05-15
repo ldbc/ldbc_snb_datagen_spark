fa443efda35354b2027ef78421eef50172cdfb34
Dávid Szakállas <david.szakallas@gmail.com>
2021-05-15 19:28:06 +0200

    use GMT tz

--- src/main/scala/ldbc/snb/datagen/SparkApp.scala
+++ src/main/scala/ldbc/snb/datagen/SparkApp.scala
@@ -1,5 +1,6 @@
 package ldbc.snb.datagen
 
+import ldbc.snb.datagen.syntax._
 import org.apache.spark.sql.SparkSession
 
 trait SparkApp {
@@ -8,6 +9,14 @@ trait SparkApp {
   implicit def spark = SparkSession
     .builder()
     .appName(appName)
+    .pipe(applySparkConf(defaultSparkConf))
     .getOrCreate()
+
+  private def applySparkConf(sparkConf: Map[String, String])(builder: SparkSession.Builder) =
+    sparkConf.foldLeft(builder) { case (b, (k, v)) => b.config(k, v) }
+
+  def defaultSparkConf: Map[String, String] = Map(
+    "spark.sql.session.timeZone" -> "GMT"
+  )
 
 }
