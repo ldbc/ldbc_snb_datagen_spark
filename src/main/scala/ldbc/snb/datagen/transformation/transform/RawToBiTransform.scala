0d377d026f3a97ba29542a0d9ddcd941a84a640e
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-01 16:03:43 +0200

    Refine condition (use lit for Boolean)

--- src/main/scala/ldbc/snb/datagen/transformation/transform/RawToBiTransform.scala
+++ src/main/scala/ldbc/snb/datagen/transformation/transform/RawToBiTransform.scala
@@ -53,7 +53,7 @@ case class RawToBiTransform(mode: BI, simulationStart: Long, simulationEnd: Long
       val idColumns = tpe.primaryKey.map(qcol)
       df
         .filter(inBatch($"deletionDate", batchStart, batchEnd))
-        .filter(if (df.columns.contains("explicitlyDeleted")) "explicitlyDeleted" else "true")
+        .filter(if (df.columns.contains("explicitlyDeleted")) col("explicitlyDeleted") else lit(true))
         .pipe(batched)
         .select(Seq($"delete_batch_id".as("batch_id"), $"deletionDate") ++ idColumns: _*)
         .repartitionByRange($"batch_id")
