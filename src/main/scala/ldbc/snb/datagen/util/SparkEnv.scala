5e45c0afedb73374f70042cd79cfcfb337628431
David Szakallas <david.szakallas@gmail.com>
2022-02-06 13:16:57 +0100

    minor nit

--- src/main/scala/ldbc/snb/datagen/util/SparkEnv.scala
+++ src/main/scala/ldbc/snb/datagen/util/SparkEnv.scala
@@ -7,7 +7,7 @@ import scala.collection.JavaConverters._
 class SparkEnv(implicit spark: SparkSession) {
   private val sysenv       = System.getenv().asScala
   private val invalidChars = raw"[.-]"
-  def env(key: String)(implicit spark: SparkSession): Option[String] = {
+  def env(key: String): Option[String] = {
     sysenv
       .get(s"LDBC_DATAGEN_${camelToUpper(key.replaceAll(invalidChars, "_"))}")
       .orElse(spark.conf.getOption(s"spark.ldbc.datagen.$key"))
