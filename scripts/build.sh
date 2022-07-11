eb450a9ebea2a5949a1dd7a50960457316b51531
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-11 11:31:54 +0200

    Add convenience scripts for building & installing sbt

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sbt assembly
---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+if [[ ! -z $(which yum) ]]; then
+    sudo rm -f /etc/yum.repos.d/bintray-rpm.repo
+    curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo
+    sudo mv sbt-rpm.repo /etc/yum.repos.d/
+    sudo yum install -y sbt
+elif [[ ! -z $(which apt-get) ]]; then
+    sudo apt-get update
+    sudo apt-get install apt-transport-https curl gnupg -yqq
+    echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+    echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+    curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+    sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+    sudo apt-get update
+    sudo apt-get install -y sbt
+else
+    echo "Operating system not supported, please install the dependencies manually"
+fi
